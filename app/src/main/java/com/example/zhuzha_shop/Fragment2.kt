a7edadf282cbcead0d01b9097b948f3d41f9c5f8
avmalyshev <tusenko64@mail.ru>
2024-01-14 01:17:17 +0300

    First commit

---
+++ app/src/main/java/com/example/zhuzha_shop/Fragment2.kt
@@ -0,0 +1,17 @@
+package com.example.zhuzha_shop
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+
+class Fragment2 : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
